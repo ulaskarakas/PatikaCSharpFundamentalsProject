675bd47f9bd7db2ab68852f3287a65a8016e1d28
ulaskarakas <ulaskarakas95@gmail.com>
2024-12-28 16:57:43 +0300

    Add second program as Calculator & add and fix some comments

--- PatikaCSharpFundamentalsProject/Program.cs
+++ PatikaCSharpFundamentalsProject/Program.cs
@@ -20,15 +20,15 @@ namespace PatikaCSharpFundamentalsProject
                     switch (option)
                     {
                         case "1":
+                            Console.WriteLine("- Random Number Finding Game -");
+                            Console.WriteLine("Welcome to the Random Number Finding Game!\nGuess a number between 1 and 100.");
+
                             //Defines variables
                             int life = 5;
 
                             // Generates random number 
                             Random random = new Random();
                             int rnd = random.Next(1, 100);
-
-                            Console.WriteLine("- Random Number Finding Game -");
-                            Console.WriteLine("Welcome to the Random Number Finding Game!\nGuess a number between 1 and 100.");
                             
                             //Wait 1 second
                             Thread.Sleep(1000);
@@ -48,11 +48,11 @@ namespace PatikaCSharpFundamentalsProject
                                     {
                                         if (guessNumber < rnd )
                                         {
-                                            Console.WriteLine($"Enter a larger number. ");
+                                            Console.WriteLine($"Enter a larger number.");
                                         }
                                         else
                                         {
-                                            Console.WriteLine($"Enter a smaller number. ");
+                                            Console.WriteLine($"Enter a smaller number.");
                                         }
                                         life--;
                                     }
@@ -64,13 +64,68 @@ namespace PatikaCSharpFundamentalsProject
                                 }
                                 else
                                 {
-
                                     Console.Write("You have entered an invalid value. Please enter an integer value.\n");
                                 }
                             }
+
                             break;
                         case "2":
                             Console.WriteLine("- Calculator -");
+                            
+                            //Define variables
+                            double firstNumber, secondNumber;
+
+                            // Asks for the first number
+                            Console.Write("Please enter the first value for the operation: ");
+                            while (!double.TryParse(Console.ReadLine(), out firstNumber)) // Geçerli bir sayı alana kadar tekrar et
+                            {
+                                Console.Write("Invalid value! Please enter a number: ");
+                            }
+
+                            // Asks for the second number
+                            Console.Write("Please enter the second value for the operation: ");
+                            while (!double.TryParse(Console.ReadLine(), out secondNumber)) // Geçerli bir sayı alana kadar tekrar et
+                            {
+                                Console.Write("Invalid value! Please enter a number: ");
+                            }
+
+                            while(true)
+                            {
+                                // // Asks for the operation
+                                Console.Write("Please select the operation you want to perform (+, -, *, /): ");
+                                string operation = Console.ReadLine();
+                                if (operation == "+" || operation == "-" || operation == "*" || operation == "/")
+                                {
+                                    switch (operation)
+                                    {
+                                        case "+":
+                                            Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+                                            break;
+                                        case "-":
+                                            Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+                                            break;
+                                        case "*":
+                                            Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+                                            break;
+                                        case "/":
+                                            if (secondNumber != 0)
+                                            {
+                                                Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
+                                            }
+                                            else
+                                            {
+                                                Console.WriteLine($"{firstNumber} cannot be divided by 0!");
+                                            }
+                                            break;
+                                    }
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.Write("You have entered an invalid value. Please enter a valid value (+, -, *, /).\n");
+                                }
+                            }
+
                             break;
                         case "3":
                             Console.WriteLine("- Average Calculation -");
